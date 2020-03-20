0b20ea32d22fef0e6d44fd7aa5d4fc14a665345a
m-lukas <mail@lukasmueller.de>
2020-03-20 11:52:29 +0100

    Unignore directories

---
+++ client/client.c
@@ -0,0 +1,46 @@
+// Client side C/C++ program to demonstrate Socket programming
+#include <stdio.h>
+#include <sys/socket.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <string.h>
+#include <arpa/inet.h>
+
+#define PORT 1234
+
+int main(int argc, char const *argv[])
+{
+    int sock = 0; long valread;
+    struct sockaddr_in serv_addr;
+    char *hello = "Hello from client";
+    char buffer[1024] = {0};
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) //socket for sending and receiving data
+    {
+        printf("\n Socket creation error \n");
+        return -1;
+    }
+    
+    memset(&serv_addr, '0', sizeof(serv_addr));
+    
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT); //💡client-socket has no own network-address
+    
+    // Convert IPv4 and IPv6 addresses from text to binary form
+    if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0)
+    {
+        printf("\nInvalid address/ Address not supported \n");
+        return -1;
+    }
+    
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) //connect to server socket
+    {
+        printf("\nConnection Failed \n");
+        return -1;
+    }
+    send(sock , hello , strlen(hello) , 0 ); //send to server
+    printf("Hello message sent\n");
+    valread = read( sock , buffer, 1024); //read from client-socket response
+    printf("%s\n",buffer );
+    return 0;
+}
