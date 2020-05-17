8a46d50e6815c0f8b30a56fca1d697562d7992b7
m-lukas <mail@lukasmueller.de>
2020-05-17 21:57:05 +0200

    WIP: Implement main thread in logger

--- logger/logger.h
+++ logger/logger.h
@@ -10,8 +10,12 @@
 #include <ctime>
 #include <sstream>
 #include <iomanip>
+#include <condition_variable>
+#include <thread>
+#include <queue>
 
 #include "../util.cpp"
+#include "../core/threadpool.h"
 
 using namespace std;
 
@@ -33,13 +37,26 @@ void SetLevel(logLevel userLvl);
 
 struct logger
 {
-    ofstream out;
-    logLevel logLevel;
+    private:
+        std::condition_variable mNotifyVar;
+        std::thread mlogThread;
+        std::mutex mNotifyMutex;
+        std::queue<std::string> mLogs;
 
-    logger(string outputPath, enum logLevel userLvl): out{ ofstream(outputPath, ios_base::app) }, logLevel{userLvl}  {}
+        void _log(std::string);
+        void startLogThread();
+        void writeToFile(logLevel lvl, std::string logMsg);
+    public:
+        ofstream mOut;
+        logLevel mLogLevel;
+
+        template<typename... Args>
+        void log(enum logLevel lvl, const char *msg, Args... args);
+
+        logger(string outputPath, enum logLevel userLvl): mOut{ ofstream(outputPath, ios_base::app) }, mLogLevel{userLvl} {}
 };
 
-logger defaultLogger = logger("log.out", WARN);
+logger defaultLogger{"log.out", WARN};
 
 string getLogTag(logLevel lvl){
     switch (lvl)
@@ -60,68 +77,99 @@ string getLogTag(logLevel lvl){
     }
 }
 
+void SetLevel(logLevel userLvl){
+    defaultLogger.mLogLevel = userLvl;
+    return;
+}
+
 template<typename... Args>
 void fatal(const char *msg, Args... args){
-    log(FATAL, msg, args...);
+    defaultLogger.log(FATAL, msg, args...);
 }
 
 template<typename... Args>
 void error(const char *msg, Args... args){
-    log(ERROR, msg, args...);
+    defaultLogger.log(ERROR, msg, args...);
 }
 
 template<typename... Args>
 void warn(const char *msg, Args... args){
-    log(WARN, msg, args...);
+    defaultLogger.log(WARN, msg, args...);
 }
 
 template<typename... Args>
 void info(const char *msg, Args... args){
-    log(INFO, msg, args...);
+    defaultLogger.log(INFO, msg, args...);
 }
 
 template<typename... Args>
 void debug(const char *msg, Args... args){
-    log(DEBUG, msg, args...);
+    defaultLogger.log(DEBUG, msg, args...);
 }
 
 template<typename... Args>
-void log(logLevel lvl, const char *msg, Args... args){
-
+void logger::log(logLevel lvl, const char *msg, Args... args){
+    std::string logMsg;
     char buffer[150]; //std::array<char, 150>
     snprintf(buffer, 150, msg, args...);
 
-    if(lvl <= defaultLogger.logLevel){
-        printf("%s %s %s\n", util::GetTimeString("%X").c_str(), getLogTag(lvl).c_str(), buffer); //printf = C Function
-    }
+    logMsg += util::GetTimeString("%Y-%m-%d %X");
+    logMsg += " ";
+    logMsg += getLogTag(lvl);
+    logMsg += " ";
+    logMsg += buffer;
 
-    switch (lvl)
     {
-        case FATAL:
-            writeToFile(lvl, buffer);
-            break;
-        case ERROR:
-            writeToFile(lvl, buffer);
-            break;
-        case WARN:
-            writeToFile(lvl, buffer);
-            break; 
-        default:
-            break;
+        std::unique_lock<std::mutex> lock{mNotifyMutex};
+        mLogs.emplace(logMsg);
+        printf("Size: %d\n", mLogs.size());
     }
+
+    mNotifyVar.notify_one();
 }
 
-void writeToFile(logLevel lvl, char *buffer){
-    if(defaultLogger.out.is_open()){
-        defaultLogger.out << util::GetTimeString("%Y-%m-%d %X").c_str() << " " << getLogTag(lvl).c_str() << " " << buffer << "\n";
-        defaultLogger.out.flush();
+void logger::_log(std::string logMsg){
+    printf("%s\n", logMsg.c_str()); //printf = C Function
+
+    /*
+    if(lvl <= logLevel){
+        printf("%s\n", logMsg); //printf = C Function
+    }
+
+    if (lvl <= WARN){
+        writeToFile(lvl, buffer);
+    }
+    */
+}
+
+void logger::writeToFile(logLevel lvl, std::string logMsg){
+    if(mOut.is_open()){
+        mOut << util::GetTimeString("%Y-%m-%d %X").c_str() << " " << getLogTag(lvl).c_str() << " " << logMsg.c_str() << "\n";
+        mOut.flush();
     }
     return;
 }
 
-void SetLevel(logLevel userLvl){
-    defaultLogger.logLevel = userLvl;
-    return;
+void logger::startLogThread(){
+    mlogThread = std::thread([=] {
+        while(true){
+            std::string logMsg;
+
+            {
+                std::unique_lock<std::mutex> lock{mNotifyMutex};
+                mNotifyVar.wait(lock, [=] { return !mLogs.empty(); });
+
+                if(mLogs.empty())
+                    break;
+
+                logMsg = std::move(mLogs.front());
+                mLogs.pop();
+            }
+
+            _log(logMsg);
+        }
+    });
+    mlogThread.detach();
 }
 
 }
